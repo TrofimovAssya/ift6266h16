25f30adde78cca863560923ef8fe09fa6b6555e8
TrofimovAssya <assya.trofimov@umontreal.ca>
2016-03-22 11:51:50 -0400

    updated dataset generation to fit specifications made in class for a common validation set

---
+++ making_dataset.py
@@ -0,0 +1,91 @@
+# processing dataset files to fit the convnet
+# the input are the files processed by image_processing.py
+# the output is one large pickle file containing the training data with all pictures and their classifications
+# as well as the pickle file containing the validation data with pictures and classifications.
+
+import sys
+import os
+import numpy
+import cPickle as pickle
+from PIL import Image
+from numpy import random
+
+
+#generating custom validation set as specified by teacher
+indices = numpy.arange(25000)
+numpy.random.RandomState(123522).shuffle(indices)
+valid = indices[-2500:]
+
+
+#obtaining the file lists
+filelist = os.listdir("processed_train")
+
+# function that processes the training sets
+def process_train(filelist,batch_index):
+	data = []
+	targets = []
+	for i in xrange (25001):
+		filename = filelist[i]
+		names = filename.split(".")
+		if not names[-1] == "jpg":
+			continue
+
+		if not int(names[1].split("_processed")[0]) % 125 == batch_index:
+		 	continue
+
+		if i in valid:
+			continue
+
+		#print filename
+
+		img = Image.open(open("/".join(["processed_train",filename])))
+		img = numpy.array(img, dtype='float32') / 256.
+		img = numpy.swapaxes(img,0,2)
+		if (names[0] == 'cat'):
+			animal = 1
+		else:
+			animal = 0
+
+
+		data.append(img)
+		targets.append(animal)
+
+	return data,targets
+
+#making a validation set
+def process_valid(vlist, filelist):
+	data = []
+	targets = []
+	for i in vlist:
+		filename = filelist[i]
+		names = filename.split(".")
+		print filename
+		img = Image.open(open("/".join(["processed_train",filename])))
+		img = numpy.array(img, dtype='float32') / 256.
+		img = numpy.swapaxes(img,0,2)
+		if (names[0] == 'cat'):
+			animal = 1
+		else:
+			animal = 0
+
+
+		data.append(img)
+		targets.append(animal)
+
+	return data,targets
+
+
+# processing the training sets 
+for batch_index in xrange(1,126,1):
+	data,targets = process_train(filelist,batch_index)
+	data = numpy.array(data)
+	targets = numpy.array(targets)
+	train_set = (data,targets)
+	pickle.dump(train_set,open(str(("_").join([str(batch_index),"dataset.p"])),'wb'))
+
+#making a validation set
+data,targets = process_valid(valid,filelist)
+data = numpy.array(data)
+targets = numpy.array(targets)
+valid_set = (data,targets)
+pickle.dump(valid_set,open("valid_dataset.p",'wb'))
